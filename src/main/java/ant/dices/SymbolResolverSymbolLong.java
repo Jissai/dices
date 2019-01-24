ce2e0413c1a5429350e71d4e65fa9401f090328b
Jissai <antapala@gmail.com>
2019-01-24 18:38:03 +0100

    Code maintenance

--- src/main/java/ant/dices/SymbolResolverSymbolLong.java
+++ src/main/java/ant/dices/SymbolResolverSymbolLong.java
@@ -12,41 +12,47 @@ public class SymbolResolverSymbolLong implements ISymbolResolver {
     public Map<String, Long> resolve(Collection<IOpposable> symbols) {
 
         Collection<IOpposable> criticalEnrichedSymbols = enrichListWithCriticalEquivalence(symbols);
-
-        // grouping
-        Map<IOpposable, Long> groupedSymbols = criticalEnrichedSymbols.stream()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<IOpposable, Long> groupedSymbols = countOccurences(criticalEnrichedSymbols);
 
         for (IOpposable key : groupedSymbols.keySet()) {
             Long currentOccurrences = groupedSymbols.get(key);
-            if (currentOccurrences == null || currentOccurrences <= 0) {
-                continue;
+            if (currentOccurrences != null && currentOccurrences > 0) {
+
+                Symbol target = new Symbol(key.getOpposite());
+                Long oppositeOccurrences = groupedSymbols.get(target);
+
+                if (oppositeOccurrences != null && oppositeOccurrences > 0) {
+                    groupedSymbols.put(target, --oppositeOccurrences);
+                    groupedSymbols.put(key, --currentOccurrences);
+                }
             }
 
-            Symbol target = new Symbol(key.getOpposite());
-            Long oppositeOccurencies = groupedSymbols.get(target);
-
-            if (oppositeOccurencies != null && oppositeOccurencies > 0) {
-                groupedSymbols.put(target, --oppositeOccurencies);
-                groupedSymbols.put(key, --currentOccurrences);
-            }
         }
 
-        Map<String, Long> result = groupedSymbols.entrySet().stream()
-            .filter(entry -> !(entry.getValue() == 0))
-            .collect(Collectors.toMap(e -> e.getKey().getId(), Map.Entry::getValue));
+        Map<String, Long> result = filterOutEmptyRecords(groupedSymbols);
 
         return result;
     }
 
     private Collection<IOpposable> enrichListWithCriticalEquivalence(Collection<IOpposable> symbols) {
         return symbols.stream()
-                .collect(ArrayList::new, (ArrayList<IOpposable> accumulator, IOpposable opposable) -> {
-                    if (opposable instanceof IOpposableEquivalence) {
-                        accumulator.addAll(((IOpposableEquivalence) opposable).getOpposableEquivalence());
-                    }
-                    accumulator.add(opposable);
-                }, ArrayList::addAll);
+            .collect(ArrayList::new, (ArrayList<IOpposable> accumulator, IOpposable item) -> {
+                if (item instanceof IOpposableEquivalence) {
+                    accumulator.addAll(((IOpposableEquivalence) item).getOpposableEquivalence());
+                }
+                accumulator.add(item);
+            }, ArrayList::addAll);
+    }
+
+    private Map<IOpposable, Long> countOccurences(Collection<IOpposable> criticalEnrichedSymbols) {
+        return criticalEnrichedSymbols.stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private Map<String, Long> filterOutEmptyRecords(Map<IOpposable, Long> groupedSymbols) {
+        return groupedSymbols.entrySet().stream()
+            .filter(entry -> !(entry.getValue() == 0))
+            .collect(Collectors.toMap(e -> e.getKey().getId(), Map.Entry::getValue));
     }
 
 }
