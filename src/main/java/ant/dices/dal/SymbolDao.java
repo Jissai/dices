6d11f6a0a3f1c77368acb63116455b3dc83160bd
Jissai <antapala@gmail.com>
2019-02-24 11:25:43 +0100

    defines toSchema/fromSchema for reuse, add list method

--- src/main/java/ant/dices/dal/SymbolDao.java
+++ src/main/java/ant/dices/dal/SymbolDao.java
@@ -8,7 +8,9 @@ import ant.dices.dal.schema.SymbolDbSchema;
 import io.jsondb.InvalidJsonDbApiUsageException;
 import io.jsondb.JsonDBTemplate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SymbolDao {
 
@@ -22,49 +24,60 @@ public class SymbolDao {
         }
     }
 
-    public void create(IOpposable opposable) {
-        SymbolDbSchema schema = new SymbolDbSchema();
-        schema.setId(opposable.getId());
-        schema.setOppositeId(opposable.getOpposite());
+    public void upsert(IOpposable opposable) {
 
         List<String> listIds = new ArrayList<>();
         if (opposable instanceof IOpposableEquivalence) {
             for (IOpposable equivalence : ((IOpposableEquivalence) opposable).getOpposableEquivalence()) {
-                this.create(equivalence);
+                this.upsert(equivalence);
                 listIds.add(equivalence.getId());
             }
         }
 
-        schema.setEquivalenceIds(listIds);
-        this.jsonDBTemplate.upsert(schema);
+        this.jsonDBTemplate.upsert(toSchema(opposable, listIds));
 
     }
 
     public IOpposable read(String id) {
         SymbolDbSchema result = this.jsonDBTemplate.findById(id, SymbolDbSchema.class);
-
-        if (result.getEquivalenceIds().size() != 0) {
-            List<IOpposable> listEquivalence = new ArrayList<>();
-            for (String equivalenceId : result.getEquivalenceIds()) {
-                listEquivalence.add(read(equivalenceId));
-            }
-            return new CriticalSymbol(
-                result.getId(),
-                result.getOppositeId(),
-                listEquivalence.toArray(new IOpposable[listEquivalence.size()]));
-
-        } else {
-            return new Symbol(result.getId(), result.getOppositeId());
-        }
+        return fromSchema(result);
     }
 
-    public void update() {
+    public Collection<IOpposable> list(){
+        List<SymbolDbSchema> results =  this.jsonDBTemplate.findAll(SymbolDbSchema.class);
+        return results.stream().map(result -> this.fromSchema(result)).collect(Collectors.toList());
     }
 
-    public void delete() {
+    public void delete(String id) {
+        SymbolDbSchema result = this.jsonDBTemplate.findById(id, SymbolDbSchema.class);
+        this.jsonDBTemplate.remove(result, SymbolDbSchema.class);
     }
 
     public void drop() {
         this.jsonDBTemplate.dropCollection(SymbolDbSchema.class);
+    }
+
+    private IOpposable fromSchema(SymbolDbSchema input) {
+        if (!input.getEquivalenceIds().isEmpty()) {
+            List<IOpposable> listEquivalence = new ArrayList<>();
+            for (String equivalenceId : input.getEquivalenceIds()) {
+                listEquivalence.add(this.read(equivalenceId));
+            }
+
+            return new CriticalSymbol(
+                input.getId(),
+                input.getOppositeId(),
+                listEquivalence.toArray(new IOpposable[listEquivalence.size()]));
+
+        }
+        return new Symbol(input.getId(), input.getOppositeId());
+    }
+
+    private SymbolDbSchema toSchema(IOpposable input, List<String> listIds) {
+        SymbolDbSchema schema = new SymbolDbSchema();
+        schema.setId(input.getId());
+        schema.setOppositeId(input.getOpposite());
+        schema.setEquivalenceIds(listIds);
+        return schema;
     }
 }
--- src/test/java/ant/dices/dal/SymbolDaoTest.java
+++ src/test/java/ant/dices/dal/SymbolDaoTest.java
@@ -5,40 +5,51 @@ import static org.assertj.core.api.Assertions.*;
 import ant.dices.CriticalSymbol;
 import ant.dices.IOpposable;
 import ant.dices.Symbol;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import java.util.Collection;
+import org.junit.jupiter.api.*;
 
 class SymbolDaoTest {
-    @Nested
-    @DisplayName("Create")
-    public class Create {
-        SymbolDao dao;
 
-        @BeforeEach
-        void setUp() {
-            dao = new SymbolDao(DbAccess.test());
-            dao.drop();
-        }
+    SymbolDao dao;
+
+    @BeforeEach
+    void setUp() {
+        dao = new SymbolDao(DbAccess.test());
+    }
+
+    @AfterEach
+    void tearDown() {
+        dao.drop();
+    }
+
+    protected void createDataSet() {
+        IOpposable symbol = new Symbol("id", "opposite");
+        IOpposable equivalence = new Symbol("opposite", "id");
+        IOpposable critical = new CriticalSymbol("crit-id", "crit-opposite", equivalence);
+        dao = new SymbolDao(DbAccess.test());
+        dao.upsert(symbol);
+        dao.upsert(critical);
+    }
+
+    @Nested
+    @DisplayName("Upsert")
+    public class Create {
 
         @Test
         @DisplayName("should write non-existing symbol in DB then read it without loss")
         void create_insert_Symbol_in_db() {
             IOpposable symbol = new Symbol("id", "opposite");
-            dao = new SymbolDao(DbAccess.test());
-            dao.create(symbol);
+            dao.upsert(symbol);
             IOpposable result = dao.read("id");
             assertThat(result).isEqualToComparingFieldByField(symbol);
         }
 
         @Test
         @DisplayName("should overwrite existing symbol in DB then read it without loss")
-        void create_duoble_insert_Symbol_in_db() {
+        void create_double_insert_Symbol_in_db() {
             IOpposable symbol = new Symbol("id", "opposite");
-            dao = new SymbolDao(DbAccess.test());
-            dao.create(symbol);
-            dao.create(symbol);
+            dao.upsert(symbol);
+            dao.upsert(symbol);
             IOpposable result = dao.read("id");
             assertThat(result).isEqualToComparingFieldByField(symbol);
         }
@@ -47,12 +58,51 @@ class SymbolDaoTest {
         @DisplayName("should write non-existing CriticalSymbol in DB then read it without loss")
         void create_insert_CriticalSymbol_in_db() {
             IOpposable equivalence = new Symbol("id", "opposite");
-            IOpposable Critical = new CriticalSymbol("crit-id", "crit-opposite", equivalence);
-            dao = new SymbolDao(DbAccess.test());
-            dao.create(Critical);
+            IOpposable critical = new CriticalSymbol("crit-id", "crit-opposite", equivalence);
+            dao.upsert(critical);
             IOpposable result = dao.read("crit-id");
-            assertThat(result).isEqualToComparingFieldByFieldRecursively(Critical);
+            assertThat(result).isEqualToComparingFieldByFieldRecursively(critical);
         }
     }
+
+    @Nested
+    @DisplayName("List")
+    protected class List {
+        @BeforeEach
+        void setUp() {
+            createDataSet();
+        }
+
+        @Test
+        @DisplayName("should list ")
+        void list_retrieve_every_element_from_db() {
+            Collection<IOpposable> results = dao.list();
+            assertThat(results).hasSize(3);
+        }
+    }
+
+//    @Nested
+//    @DisplayName("Remove")
+//    protected class Remove {
+//        SymbolDao dao;
+//
+//        @BeforeEach
+//        void setUp() {
+//            createDataSet();
+//        }
+//
+//        @Test
+//        @DisplayName("should remove existing symbol in DB")
+//        void remove_existing_symbol_in_db() {
+//
+//        }
+//
+//        @Test
+//        @DisplayName("should remove existing CriticalSymbol in DB. Leave references")
+//        void remove_existing_CriticalSymbol_in_db() {
+//
+//        }
+//
+//    }
 
 }
